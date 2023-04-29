63ff34a5e0a5116931f96ac08d5d403928978670
Zytronik <omar.oberholzer@gmail.com>
2023-04-29 15:18:16 +0200

    ready up data

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ window.onload = function () {
 
 function handleFormData() {
     let nameValue = document.getElementById("name").value;
+
+    let age = document.getElementById("age").value;
+
     let gender_menValue = document.getElementById("gender_men").checked;
     let gender_womenValue = document.getElementById("gender_women").checked;
     let gender;
@@ -18,25 +21,51 @@ function handleFormData() {
     } else {
         gender = "female";
     }
-    let tclass_1Value = document.getElementById("tclass_1").checked;
-    let tclass_2Value = document.getElementById("tclass_2").checked;
-    let tclass_3Value = document.getElementById("tclass_3").checked;
+
+    let tclass_1Value = document.getElementById("tclass_1");
+    let tclass_2Value = document.getElementById("tclass_2");
+    let tclass_3Value = document.getElementById("tclass_3");
     let tclass;
-    if(tclass_1Value && !tclass_2Value && !tclass_3Value){
-        tclass = "vip";
-    }else if(!tclass_1Value && tclass_2Value && !tclass_3Value){
-        tclass = "firstClass";
+    if(tclass_1Value.checked && !tclass_2Value.checked && !tclass_3Value.checked){
+        tclass = tclass_1Value.value;
+    }else if(!tclass_1Value.checked && tclass_2Value.checked && !tclass_3Value.checked){
+        tclass = tclass_2Value.value;
     }else{
-        tclass = "economic";
+        tclass = tclass_3Value.value;
     }
+
     let priceValue = document.getElementById("price").value;
     
+    var cabinSector = document.getElementById("cabinSector");
+    var cabinSectorValue = cabinSector.options[cabinSector.selectedIndex].text;
+
+    let sib_sp = document.getElementById("sib_sp").value;
+
+    let child_par = document.getElementById("child_par").value;
+
+    let port_1Value = document.getElementById("port_cherbourg");
+    let port_2Value = document.getElementById("port_queenstown");
+    let port_3Value = document.getElementById("port_southampton");
+    let port;
+    if(port_1Value.checked && !port_2Value.checked && !port_3Value.checked){
+        port = port_1Value.value;
+    }else if(!port_1Value.checked && port_2Value.checked && !port_3Value.checked){
+        port = port_2Value.value;
+    }else{
+        port = port_3Value.value;
+    }
+
 
     let passenger = {
         "name": nameValue,
+        "age": age,
         "gender": gender,
         "tclass": tclass,
         "price": priceValue,
+        "cabinSector": cabinSectorValue,
+        "sib_sp": sib_sp,
+        "child_par": child_par,
+        "port": port,
     };
 
     console.log(passenger);
